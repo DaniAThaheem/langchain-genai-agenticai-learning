500c22d167e6ea35aa8f99f427ea901c796fa481
DaniAThaheem <danimsbzuacc@outlook.com>
2026-09-03 17:37:32 +0500

    Improved the project by raw wiring of RAG

--- Langchain/Components/1_Models/project/document_similarity_app_without_rag.py
+++ Langchain/Components/1_Models/project/document_similarity_app_without_rag.py
@@ -1,10 +1,11 @@
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 from dotenv import load_dotenv
 from sklearn.metrics.pairwise import cosine_similarity
 
 
 load_dotenv()
 
+model = ChatGoogleGenerativeAI(model="gemini-flash-latest")
 embeddings = GoogleGenerativeAIEmbeddings(model="gemini-embedding-2")
 documents =[
     "Shahid Afridi is pakistan all rounder cricketer. Also know as Lala",
@@ -13,7 +14,7 @@ documents =[
 ]
 doc_embeddings = embeddings.embed_documents(documents)
 
-query = "Who has three personalities, cricketer, prime minister and politician?"
+query = "Which figure led the cricket team, then later ran the government and got arrested?"
 query_embeddings = embeddings.embed_query(query)
 sorted_embeddings = sorted(enumerate(cosine_similarity([query_embeddings], doc_embeddings)[0]), key=lambda x: x[1])
 print(sorted_embeddings)
@@ -21,3 +22,6 @@ index, score = sorted_embeddings[-1]
 print(query)
 print(documents[index])
 print("embedding socre", score)
+
+result = model.invoke("query="+query +" and context="+documents[index])
+print(result.content)
