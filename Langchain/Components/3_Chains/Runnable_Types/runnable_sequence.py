970c24a3b5b51f8b05baf3141aa8fa8c417d22ea
DaniAThaheem <danimsbzuacc@outlook.com>
2026-09-10 19:47:13 +0500

    Implemented the use case of the runnable sequence

---
+++ Langchain/Components/3_Chains/Runnable_Types/runnable_sequence.py
@@ -0,0 +1,22 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+from langchain_core.runnables import RunnableSequence
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv()
+
+model = ChatGoogleGenerativeAI(model="gemini-flash-latest")
+
+template = PromptTemplate(
+    template="Predict the future about the give technology {technology}",
+    input_variables=["technology"]
+)
+
+parser = StrOutputParser()
+
+chain = RunnableSequence(model, template, parser)
+
+result = chain.invoke({"technology": "AI"})
+
+print(result)
