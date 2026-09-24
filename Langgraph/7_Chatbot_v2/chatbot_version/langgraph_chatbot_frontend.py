d28dcb8a9254492e4936cc84174e2908f70d6297
DaniAThaheem <danimsbzuacc@outlook.com>
2026-09-24 09:05:10 +0500

    Implemented the backend and setup  of chatbot with the frontend

--- Langgraph/7_Chatbot_v2/chatbot_version/langgraph_chatbot_frontend.py
+++ Langgraph/7_Chatbot_v2/chatbot_version/langgraph_chatbot_frontend.py
@@ -1,6 +1,15 @@
 import streamlit as st
+from langgraph_chatbot_backend import workflow
+from langchain_core.messages import HumanMessage
 
 user_input = st.chat_input("Type here...")
+
+thread_id = "123abc"
+config = {
+        "configurable":{
+            "thread_id":thread_id
+        }
+    }
 
 if "message_history" not in st.session_state:
     st.session_state["message_history"] = []
@@ -14,10 +23,12 @@ if user_input:
     with st.chat_message("user"):
         st.session_state["message_history"].append({"role":"human", "content":user_input})
         st.write(user_input)
+        init_state = {"messages":[HumanMessage(content=input)]}
+        ai_output = workflow.invoke(init_state, config=config)
 
     with st.chat_message("assistant"):
-        st.session_state["message_history"].append({"role":"assistant", "content":user_input})
-        st.write(user_input)
+        st.session_state["message_history"].append({"role":"assistant", "content":ai_output[-1].content})
+        st.write(ai_output[-1].content)
 
 
     
