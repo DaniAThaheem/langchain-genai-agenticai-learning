854b7ff4b7e908b26173361ce34487aece0a8d50
DaniAThaheem <danimsbzuacc@outlook.com>
2026-09-25 20:07:27 +0500

    Implemented langsmith about sequential chain

---
+++ Langgraph/13_Langsmith/2_practice.py
@@ -0,0 +1,25 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv()
+
+llm = ChatGoogleGenerativeAI(model="gemini-flash-latest")
+
+prompt1 = PromptTemplate(
+    template="You are a professional writter about the Growth in Artifical Intelligence field. Write a long article meaningful about the topic {topic}.",
+    input_variables=["topic"]
+)
+prompt2 = PromptTemplate(
+    template="You are a professional writter who summarize the topic in a meaningful way about the Growth in Artifical Intelligence field. Write a summary about the article meaningful about the article {text}.",
+    input_variables=["text"]
+)
+
+parser = StrOutputParser()
+
+chain = prompt1 | llm | parser | prompt2 | llm | parser
+
+result = chain.invoke({"topic":"Masters in Artifical Intelligence"})
+
+print(result)
