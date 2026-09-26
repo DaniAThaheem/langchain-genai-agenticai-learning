99083a35a47e5b1ddb560363f43d4b118bf987bc
DaniAThaheem <danimsbzuacc@outlook.com>
2026-09-26 21:25:37 +0500

    Implemented langsmith for the agents

---
+++ Langgraph/13_Langsmith/6_practice.py
@@ -0,0 +1,72 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.tools import tool
+import requests
+from langchain_community.tools import DuckDuckGoSearchRun
+from langchain_classic.agents import create_react_agent, AgentExecutor
+from langchain_core.prompts import PromptTemplate
+from dotenv import load_dotenv
+
+load_dotenv()
+
+search_tool = DuckDuckGoSearchRun()
+
+@tool
+def get_weather_data(city: str) -> str:
+  """
+  This function fetches the current weather data for a given city
+  """
+  url = f'https://api.weatherstack.com/current?access_key=2d1e039103e1c73bcfe3680de45ec022&query={city}'
+
+  response = requests.get(url)
+
+  return response.json()
+
+llm = ChatGoogleGenerativeAI(model="gemini-flash-latest")
+
+# Step 2: Pull the ReAct prompt from LangChain Hub
+prompt = PromptTemplate.from_template(template="""
+Answer the following questions as best you can. You have access to the following tools:
+
+{tools}
+
+Use the following format:
+
+Question: the input question you must answer
+Thought: you should always think about what to do
+Action: the action to take, should be one of [{tool_names}]
+Action Input: the input to the action
+Observation: the result of the action
+... (this Thought/Action/Action Input/Observation can repeat N times)
+Thought: I now know the final answer
+Final Answer: the final answer to the original input question
+
+Begin!
+
+Question: {input}
+Thought:{agent_scratchpad}
+""")  # pulls the standard ReAct agent prompt
+
+# Step 3: Create the ReAct agent manually with the pulled prompt
+agent = create_react_agent(
+    llm=llm,
+    tools=[search_tool, get_weather_data],
+    prompt=prompt
+)
+
+# Step 4: Wrap it with AgentExecutor
+agent_executor = AgentExecutor(
+    agent=agent,
+    tools=[search_tool, get_weather_data],
+    verbose=True,
+    max_iterations=5
+)
+
+# What is the release date of Dhadak 2?
+# What is the current temp of gurgaon
+# Identify the birthplace city of Kalpana Chawla (search) and give its current temperature.
+
+# Step 5: Invoke
+response = agent_executor.invoke({"input": "What is the current temp of gurgaon"})
+print(response)
+
+print(response['output'])
